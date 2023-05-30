c299f5a0cd9913731027b78fe3b9f1577bfcf607
Peder Goodman <peder.goodman@gmail.com>
2023-05-30 14:11:38 -0700

    triangle challenge

--- practice.js
+++ practice.js
@@ -1,1 +1,12 @@
 console.log('JS Loops Practice');
+
+
+// Triangle loop
+console.log('Triangle Loop');
+let triangle = ''
+for (let i = 1; i < 8; i++) {
+    triangle += '#'
+    console.log(triangle);
+}
+
+
